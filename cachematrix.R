0db4628353a17e0fc56264ab83bf1b9bf44555bd
blakewheeler89 <blakewheeler89@gmail.com>
2019-06-04 19:13:44 -0400

    refactored and updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,38 @@
 #creates a special matrix object that can cache its inverse  
 makeCacheMatrix <- function(x = matrix()) {
-  original <- NULL
   inverse <- NULL
+  
+  #set/modify the value of the matrix, 'reset' the stored value of inverse; get the value of the matrix
   set <- function(y) {
     x <<- y
-    inverse <<- NULL
+    inverse <<- NULL #sets inverse back to NULL if the matrix is changed
   }
   get <- function() x
-  setoriginal <- function(y) {
-    original <<- y
-  }
-  getoriginal <- function() original
+  
+  #set/cache the value of inverse; get the value of inverse
   setinverse <- function(inverse) inverse <<- inverse
   getinverse <- function() inverse
-  list(set = set, get = get, getoriginal = getoriginal, setoriginal = setoriginal,
+  
+  #return an object of type makeCacheMatrix to be used by cacheSolve
+  list(set = set, get = get, 
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
 
-#computes the inverse of the special matrix returned by makeCacheMatrix
-#if the inverse has already been calculated (and the matrix has not changed), then retrieve the inverse from the cache
-cacheSolve <- function(x, ...) {
-  original <- x$getoriginal()
-  # Return a matrix that is the inverse of 'x'
+#computes the inverse of the matrix returned by makeCacheMatrix
+cacheSolve <- function(x) {
+  #get the value stored 
   inverse <- x$getinverse()
-  if(!is.null(inverse) && !is.null(original) && all(original == x$get())) {
-    message("getting cached data")
+  
+  #if the inverse in NOT null, then retrieve the inverse from the cache
+  if(!is.null(inverse)) { 
+    message("cached inverse of matrix")
     return(inverse)
   }
+  #get & store the matrix, get & store the inverse of the matrix, cache the inverse, print the result 
   data <- x$get()
-  x$setoriginal(data) #save the original
-  m <- solve(data)
-  x$setinverse(m)
-  m
+  inverse <- solve(data)
+  x$setinverse(inverse)
+  inverse
 }
